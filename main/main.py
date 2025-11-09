cbcb6b74e5dac5e38c14f818e95c74b9de3a7f7a
Iris Cheung <hokyinirischeung@gmail.com>
2025-11-08 23:45:24 -0800

    completed and all test passed

--- main/main.py
+++ main/main.py
@@ -1,7 +1,24 @@
 def reshape_matrix(nums, rows, columns):
-    pass
+    new_shape = []
+    temp_list = []
+
+    for matrix in nums:
+        for n in matrix:
+            temp_list.append(n)
+    
+    if check_valid(temp_list, rows, columns):
+        run = 0
+        start = 0
+        while run != rows:
+            row_nums = temp_list[start: start + columns]
+            new_shape.append(row_nums)
+            start += columns
+            run += 1
+        return new_shape
 
 
-        
-        
+def check_valid(temp_list, rows, columns):
+    if len(temp_list) != rows * columns:
+        raise ValueError("Matrix cannot reshape")
+    return True
 
--- test/test_main.py
+++ test/test_main.py
@@ -1,37 +1,111 @@
 from main.main import reshape_matrix
+import pytest
 
-def test_reshape_matrix_2_2(nums, rows, columns):
+def test_reshape_matrix_2_2():
     # arrange
-    nums = 
-    [[1, 2],
-    [3, 4]]
+    nums = [[1, 2],[3, 4]]
     rows = 1
     columns = 4
     
     result = reshape_matrix(nums, rows, columns)
     
     assert result == [[1, 2, 3, 4]]
-    
-def test_reshape_rows_error(nums, rows, columns):
-    nums = 
-    [[1, 2],
-    [3, 4]]
+
+
+def test_reshape_3_2():
+    nums = [[1, 2],[3, 4],[5, 6]]
     rows = 2
-    columns = 4
-    
-    result = reshape_matrix(nums, rows, columns)
-    
-    with pytest.raises(ValueError, match="Matrix cannot reshape"):
-        reshape_matrix(nums, rows, columns)
-
-
-def test_reshape_rows_error(nums, rows, columns):
-    nums = 
-    [[1, 2],
-    [3, 4],
-    [5, 6]]
-    rows = 2, columns = 3
+    columns = 3
     
     result = reshape_matrix(nums, rows, columns)
     
     assert result == [[1, 2, 3], [4, 5, 6]]
+
+
+def test_reshape_cannot_reshape():
+    nums = [[1, 2], [3, 4]]
+    rows = 2
+    columns = 4
+    
+    with pytest.raises(ValueError):
+        reshape_matrix(nums, rows, columns)
+
+
+def test_reshpae_1_1():
+    nums = [[1]]
+    rows = 1
+    columns = 1
+
+    result = reshape_matrix(nums, rows, columns)
+    
+    assert result == [[1]]
+
+
+def test_reshape_cannot_reshape_raise_message():
+    nums = [[1, 2, 3], [4, 5, 6]]
+    rows = 2
+    columns = 4
+    
+    with pytest.raises(ValueError):
+        reshape_matrix(nums, rows, columns)
+
+
+
+
+def test_reshape_matrix_1_by_4_can_reshape_into_4_by_1():
+    # Arrange
+    nums = [[1], [2], [3], [4]]
+    rows = 1
+    columns = 4
+
+    # Act
+    result = reshape_matrix(nums, rows, columns)
+
+    # Assert
+    assert result == [[1, 2, 3, 4]]
+
+def test_reshape_matrix_2_by_2_can_reshape_into_1_by_4():
+    # Arrange
+    nums = [[1, 2], [3, 4]]
+    rows = 1
+    columns = 4
+
+    # Act
+    result = reshape_matrix(nums, rows, columns)
+
+    # Assert
+    assert result == [[1, 2, 3, 4]]
+
+def test_reshape_matrix_4_by_2_can_reshape_into_2_by_4():
+    # Arrange
+    nums = [[1, 2], [3, 4], [5, 6], [7, 8]]
+    rows = 2
+    columns = 4
+
+    # Act
+    result = reshape_matrix(nums, rows, columns)
+
+    # Assert
+    assert result == [[1, 2, 3, 4], [5, 6, 7, 8]]
+
+def test_reshape_matrix_3_by_3_can_reshape_into_9_by_1():
+    # Arrange
+    nums = [[7, 2, 1], [4, 3, 5], [6, 9, 8]]
+    rows = 9
+    columns = 1 
+
+    # Act
+    result = reshape_matrix(nums, rows, columns)
+
+    # Assert
+    assert result == [[7], [2], [1], [4], [3], [5], [6], [9], [8]]
+
+def test_reshape_matrix_2_by_2_raises_error_reshaping_to_4_by_2():
+    # Arrange
+    nums = [[1,2], [3,4]]
+    rows = 4
+    columns = 2
+
+    # Act & Assert
+    with pytest.raises(ValueError):
+        reshape_matrix(nums, rows, columns)
